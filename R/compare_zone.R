5eec9fb06251c2326a247e05242a811614888241
PPaccioretti <ppaccioretti2@unl.edu>
2025-05-01 08:50:24 -0500

    Transform to centroids for spatial validarion

--- R/compare_zone.R
+++ R/compare_zone.R
@@ -1,4 +1,3 @@
-
 #' Compare spatial zone means
 #'
 #' @param data \code{sf} object with zones
@@ -20,69 +19,75 @@
 #' @example inst/examples/compare_zone.R
 #'
 
-compare_zone <- function(data,
-                         variable,
-                         zonesCol,
-                         alpha = 0.05,
-                         join = sf::st_nearest_feature,
-                         returnLSD = FALSE,
-                         grid_dim) {
-
-  descriptive <- varKrigDescr(data,
-                              variable,
-                              grid_dim = grid_dim)
+compare_zone <- function(
+  data,
+  variable,
+  zonesCol,
+  alpha = 0.05,
+  join = sf::st_nearest_feature,
+  returnLSD = FALSE,
+  grid_dim
+) {
+  descriptive <- varKrigDescr(data, variable, grid_dim = grid_dim)
 
   comparisson_table <-
-    validVarKrig(data,
-                 variable,
-                 zonesCol,
-                 descriptive,
-                 returnLSD = returnLSD,
-                 join = join)
+    validVarKrig(
+      data,
+      variable,
+      zonesCol,
+      descriptive,
+      returnLSD = returnLSD,
+      join = join
+    )
 
-  list('differences' = comparisson_table$Diferencias,
-       'descriptive_stat' = comparisson_table$Descriptivo)
-
+  list(
+    'differences' = comparisson_table$Diferencias,
+    'descriptive_stat' = comparisson_table$Descriptivo
+  )
 }
 
 #' Auxiliary function for zone compare
 #'
 #' @noRd
 validVarKrig <-
-  function(clusterData,
-           dataToCompare,
-           numCluster,
-           descStat,
-           join = sf::st_nearest_feature,
-           returnLSD) {
-
+  function(
+    clusterData,
+    dataToCompare,
+    numCluster,
+    descStat,
+    join = sf::st_nearest_feature,
+    returnLSD
+  ) {
     MedCV <-
       paste0(round(descStat$MediaVar, 2), " (", round(descStat$CVVar, 1), ")")
     MedCV <-
       data.table::data.table(
         "Variable" = descStat$Variable,
-                 MedCV,
-                 "n" = descStat$n)
+        MedCV,
+        "n" = descStat$n
+      )
 
-    descStatTable <- data.table::dcast(data = MedCV,
-                          n ~ Variable ,
-                          value.var = "MedCV")
+    descStatTable <- data.table::dcast(
+      data = MedCV,
+      n ~ Variable,
+      value.var = "MedCV"
+    )
 
     ##### ComparacionMedias ----
 
     if (inherits(dataToCompare, "sf")) {
       MeansComparissons <-
         lapply(colnames(sf::st_drop_geometry(dataToCompare)), function(x) {
-          makeMeanComparisson(dataToCompare[,x],
-                              Clasif = clusterData,
-                              numCluster = numCluster,
-                              descStat = descStat,
-                              join = join,
-                              retDMS = returnLSD)
-
+          makeMeanComparisson(
+            dataToCompare[, x],
+            Clasif = clusterData,
+            numCluster = numCluster,
+            descStat = descStat,
+            join = join,
+            retDMS = returnLSD
+          )
         })
       names(MeansComparissons) <- colnames(sf::st_drop_geometry(dataToCompare))
-
     } else {
       MeansComparissons <-
         lapply(
@@ -97,19 +102,14 @@ validVarKrig <-
       names(MeansComparissons) <- dataToCompare
     }
 
-
-
-    return(list(Diferencias = MeansComparissons,
-                Descriptivo = descStatTable))
+    return(list(Diferencias = MeansComparissons, Descriptivo = descStatTable))
   }
 
 #' Auxiliary function for zone compare
 #'
 #' @noRd
 varKrigDescr <-
-  function(datos_predsf,
-           dataToCompare,
-           grid_dim) {
+  function(datos_predsf, dataToCompare, grid_dim) {
     is_newData <- FALSE
     # If variable is not from the same dataset, an interpolation is made assuming that
     # new data is not interpolated.
@@ -121,10 +121,9 @@ varKrigDescr <-
       datos_predsf <- dataToCompare
       dataToCompare <- colnames(sf::st_drop_geometry(dataToCompare))
     }
-    if (missing(grid_dim))
-      grid_dim <- NULL
+    if (missing(grid_dim)) grid_dim <- NULL
     Krig <-
-      lapply(dataToCompare, function(columna) {
+      lapply(dataToCompare, function() {
         if (columna == "geometry") {
           return(NULL)
         }
@@ -137,47 +136,46 @@ varKrigDescr <-
         # Ajuste de semivariograma teóricos sin valores iniciales de los parámetros
 
         suppressWarnings({
-           mdls <-
-          gstat::fit.variogram(semiv_emp, gstat::vgm(c("Exp", "Sph", "Gau")))
+          mdls <-
+            gstat::fit.variogram(semiv_emp, gstat::vgm(c("Exp", "Sph", "Gau")))
 
-           if (is_newData) {
-             if (is.null(grid_dim)) {
-               grid_dim <-  mean(diff(sf::st_bbox(datos_predsf),lag = 2)/100)
-             }
-             if (requireNamespace("stars", quietly = TRUE)) {
-               grd <- sf::st_bbox(datos_predsf)
-               grd <- stars::st_as_stars(grd, dx = grid_dim)
-               grd <- sf::st_crop(grd, datos_predsf)
+          if (is_newData) {
+            if (is.null(grid_dim)) {
+              grid_dim <- mean(diff(sf::st_bbox(datos_predsf), lag = 2) / 100)
+            }
+            if (requireNamespace("stars", quietly = TRUE)) {
+              grd <- sf::st_bbox(datos_predsf)
+              grd <- stars::st_as_stars(grd, dx = grid_dim)
+              grd <- sf::st_crop(grd, datos_predsf)
 
-               kriging_cv <-
-                 gstat::krige(formulaKr,
-                              datos_predsf,
-                              grd,
-                              mdls,
-                              nmin = 7,
-                              nmax = 25)
-             } else {
-               message("stars package is suggested to do this comparisson")
-               is_newData <- !is_newData
-             }
+              kriging_cv <-
+                gstat::krige(
+                  formulaKr,
+                  datos_predsf,
+                  grd,
+                  mdls,
+                  nmin = 7,
+                  nmax = 25
+                )
+            } else {
+              message("stars package is suggested to do this comparisson")
+              is_newData <- !is_newData
+            }
+          }
+          if (!is_newData) {
+            kriging_cv <-
+              gstat::krige.cv(
+                formula = formulaKr,
+                locations = sf::st_centroid(datos_predsf),
+                nfold = 10,
+                nmin = 7,
+                nmax = 25,
+                model = mdls
+              )
+          }
 
-
-           }
-           if (!is_newData) {
-             kriging_cv <-
-               gstat::krige.cv(
-                 formulaKr,
-                 datos_predsf,
-                 nfold = 10,
-                 nmin = 7,
-                 nmax = 25,
-                 model = mdls
-               )
-           }
-
-        medianKriging <- stats::median(sqrt(kriging_cv$var1.var), na.rm = T)
+          medianKriging <- stats::median(sqrt(kriging_cv$var1.var), na.rm = T)
         })
-
 
         media <- mean(datos_predsf[[columna]], na.rm = T)
         Cv <-
@@ -190,7 +188,6 @@ varKrigDescr <-
           "CVVar" = Cv,
           "n" = tamPunt
         )
-
       })
 
     descStat <- do.call(rbind, Krig)
@@ -201,31 +198,37 @@ varKrigDescr <-
 #'
 #' @noRd
 makeMeanComparisson <-
-  function(VariableEstudiada,
-           Clasif,
-           numCluster,
-           descStat = descStat,
-           alpha = 0.05,
-           join = sf::st_nearest_feature,
-           retDMS = FALSE) {
-
+  function(
+    VariableEstudiada,
+    Clasif,
+    numCluster,
+    descStat = descStat,
+    alpha = 0.05,
+    join = sf::st_nearest_feature,
+    retDMS = FALSE
+  ) {
     if (inherits(VariableEstudiada, "sf")) {
-
-      myData <- sf::st_join(VariableEstudiada,
-                            Clasif[, numCluster],
-                            join = join)
+      myData <- sf::st_join(
+        VariableEstudiada,
+        Clasif[, numCluster],
+        join = join
+      )
       medias <- stats::aggregate(
         myData[, colnames(myData) != numCluster],
         list(myData[[numCluster]]),
-        mean, na.rm = TRUE)
+        mean,
+        na.rm = TRUE
+      )
       VariableEstudiada <- colnames(sf::st_drop_geometry(VariableEstudiada))
     } else {
       medias <-
-        stats::aggregate(Clasif[,VariableEstudiada],
-                         list(Clasif[[numCluster]]),
-                         mean, na.rm = TRUE)
+        stats::aggregate(
+          Clasif[, VariableEstudiada],
+          list(Clasif[[numCluster]]),
+          mean,
+          na.rm = TRUE
+        )
     }
-
 
     names(medias)[1] <- c(numCluster)
     medias <- medias[order(medias[[VariableEstudiada]], medias[[numCluster]]), ]
@@ -240,20 +243,21 @@ makeMeanComparisson <-
 
     DMS <-
       descStat[which(descStat[["Variable"]] == VariableEstudiada), "MedianaK"] *
-      stats::qnorm(1 - alpha / 2)# * 2 It is too much?? Why is 2 here??
+      stats::qnorm(1 - alpha / 2) # * 2 It is too much?? Why is 2 here??
 
     for (k in 1:nn) {
       i <- comb[1, k]
       j <- comb[2, k]
       dif[k] <- medias[i, ][[2]] - medias[j, ][[2]]
-      sdtdif[k] <- descStat[which(descStat[["Variable"]] == VariableEstudiada), "MedianaK"]
+      sdtdif[k] <- descStat[
+        which(descStat[["Variable"]] == VariableEstudiada),
+        "MedianaK"
+      ]
       pvalue[k] <- abs(dif[k][[1]]) <= DMS
     }
 
     MisLetras <-
-      CalcLetras(myMeans = medias,
-                 pvalue = pvalue,
-                 alpha = alpha)
+      CalcLetras(myMeans = medias, pvalue = pvalue, alpha = alpha)
     if (retDMS) {
       return(list("TablaMedias" = MisLetras, "DMS" = DMS))
     }
@@ -283,14 +287,37 @@ CalcLetras <- function(myMeans, pvalue, alpha = 0.05) {
     }
   }
 
-
-
   n <- nrow(myMeans)
   z <- myMeans
-  letras <- c(letters[1:26],LETTERS[1:26],1:9,c(".","+","-","*","/","#","$",
-                                                "%","&","^","[","]",":","@",
-                                                ";","_","?","!","=","#",rep(" ",2000)))
-  w <- z[order(z[[2]], decreasing = FALSE),]
+  letras <- c(
+    letters[1:26],
+    LETTERS[1:26],
+    1:9,
+    c(
+      ".",
+      "+",
+      "-",
+      "*",
+      "/",
+      "#",
+      "$",
+      "%",
+      "&",
+      "^",
+      "[",
+      "]",
+      ":",
+      "@",
+      ";",
+      "_",
+      "?",
+      "!",
+      "=",
+      "#",
+      rep(" ", 2000)
+    )
+  )
+  w <- z[order(z[[2]], decreasing = FALSE), ]
   M <- rep("", n)
   k <- 1
   # k1 <- 0
@@ -303,41 +330,34 @@ CalcLetras <- function(myMeans, pvalue, alpha = 0.05) {
   q <- as.numeric(rownames(w)) #Check
   while (j < n) {
     chequeo <- chequeo + 1
-    if (chequeo > n)
-      break
+    if (chequeo > n) break
     for (i in j:n) {
       stest <- Qm[q[i], q[j]] > alpha
       if (stest) {
         if (lastC(M[i]) != letras[k]) {
           M[i] <- paste(M[i], letras[k], sep = "")
         }
-      }
-      else {
+      } else {
         k <- k + 1
         cambio <- i
         cambio1 <- 0
         ja <- j
-        for (jj in cambio:n)
-          M[jj] <- paste(M[jj], "", sep = "")
+        for (jj in cambio:n) M[jj] <- paste(M[jj], "", sep = "")
         M[cambio] <- paste(M[cambio], letras[k], sep = "")
         for (v in ja:cambio) {
           if (Qm[q[v], q[cambio]] <= alpha) {
             j <- j + 1
             cambio1 <- 1
-          }
-          else
-            break
+          } else break
         }
         break
       }
     }
-    if (cambio1 == 0)
-      j <- j + 1
+    if (cambio1 == 0) j <- j + 1
   }
 
   w <- data.frame(w, stat = M)
-  if (k > 81
-  )
+  if (k > 81)
     warning(
       "\n",
       k,
@@ -345,4 +365,3 @@ CalcLetras <- function(myMeans, pvalue, alpha = 0.05) {
     )
   invisible(w)
 }
-
